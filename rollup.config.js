734e69217dff57b3f8a44d1ed89920f3b57d4564
jonasbn <jbn@domutech.dk>
2025-10-09 17:12:56 +0200

    Attempting to use this will rollup as recommended and outlined in the documentation: https://docs.github.com/en/actions/tutorials/create-actions/create-a-javascript-action

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+// REF: https://docs.github.com/en/actions/tutorials/create-actions/create-a-javascript-action
+
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+const config = {
+  input: "src/index.js",
+  output: {
+    esModule: true,
+    file: "dist/index.js",
+    format: "es",
+    sourcemap: true,
+  },
+  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+};
+
+export default config;
